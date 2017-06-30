cd3d8d791da159bb8f390e42b3509cea7144f3b6
bapjiws <alex.boklin@gmail.com>
2017-06-30 18:00:52 +0300

    Set up Todo model and its migration

--- todo_list/api/models.py
+++ todo_list/api/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    priority = models.IntegerField(default=0)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
