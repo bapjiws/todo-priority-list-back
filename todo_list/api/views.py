4876a267afaa587d579dcf21a33ad10484b9b75b
bapjiws <alex.boklin@gmail.com>
2017-06-30 22:15:39 +0300

    [WIP] Serialize JSON in loadTodos

--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.core.serializers import serialize
+from django.core.serializers.json import DjangoJSONEncoder
 
 import json
 
@@ -16,13 +16,33 @@ def index(request):
 
 def loadTodos(request):
     if request.method == "GET":
-        return HttpResponse(serialize('json', Todo.objects.all()))
+        # resp = [];
+        # for t in Todo.objects.all():
+        #     print(t.description)
+        #     print(t.name)
+        #
+        #     resp.append(json.dumps({ t.priority, t.name,t.description } ))
+        resp = Todo.objects.all().values()
+        # print(list(resp))
+        resp_json = json.dumps(list(resp), ensure_ascii=False, default=str)
+        # print(resp_json)
+
+        # class Encoder(DjangoJSONEncoder):
+        #     def default(self, o):
+        #         return json.dumps(list(o), ensure_ascii=False, default=str)
+
+        # return JsonResponse(resp_json, safe=False)
+        return HttpResponse(resp_json)
 
 @csrf_exempt # probably better to add localhost to CSRF_TRUSTED_ORIGINS
 def addTodo(request):
     if request.method == "POST":
         payload = json.loads(request.body)
-        todo = Todo.objects.create(priority=payload['priority'], name=payload['name'], description=payload['description'])
+        todo = Todo.objects.create(
+            priority=payload['priority'],
+            name=unicode(payload['name']),
+            description=payload['description']
+        )
         todo.save()
         return HttpResponse(request.body)
 
