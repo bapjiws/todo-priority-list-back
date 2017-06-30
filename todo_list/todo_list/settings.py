76d339e6a54eadc48bc6c66e6f186b817e20c896
bapjiws <alex.boklin@gmail.com>
2017-06-30 17:23:19 +0300

    Set up db access for PostgreSQL

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import getpass # Needed to get the username for db access
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_list',
+        'USER': getpass.getuser(),
+        'PASSWORD': 'letmein',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
